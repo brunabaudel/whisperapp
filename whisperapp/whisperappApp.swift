7b980ac1b7c8893cd553f5caeecf8bdc864c5c87
Bruna Baudel <brubaudel@gmail.com>
2023-01-21 18:10:04 +0100

    Initial Commit

---
+++ whisperapp/whisperappApp.swift
@@ -0,0 +1,17 @@
+//
+//  whisperappApp.swift
+//  whisperapp
+//
+//  Created by Bruna Baudel on 21/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct whisperappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
